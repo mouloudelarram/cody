88c3b7c69911dfc0fa6d53be7f236b0ae2f8b9af
mouloudelarram <maoiloud2002@gmail.com>
2026-03-01 16:18:15 +0100

    fix: scripts

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# run_gunicorn.sh
-# Navigate to your app directory
 
-# File to store PID
 PIDFILE="gunicorn.pid"
+LOGFILE="gunicorn.log"
 
-# Run gunicorn in the background
-nohup gunicorn -w 4 -b 127.0.0.1:5000 app:app > gunicorn.log 2>&1 &
+gunicorn -w 4 -b 127.0.0.1:5000 \
+    --daemon \
+    --pid "$PIDFILE" \
+    app:app \
+    > "$LOGFILE" 2>&1
 
-# Save the PID
-echo $! > "$PIDFILE"
-echo "Gunicorn started in the background with PID $(cat $PIDFILE)"
--- stop_gunicorn.sh
+++ stop_gunicorn.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-PIDFILE="gunicorn.pid"
 
-if [ -f "$PIDFILE" ]; then
-    PID=$(cat "$PIDFILE")
-    echo "Stopping Gunicorn with PID $PID"
-    kill "$PID"
-    rm "$PIDFILE"
-else
-    echo "PID file not found. Is Gunicorn running?"
-fi
+echo "kill all gunicorn process"
+
+pkill gunicorn
